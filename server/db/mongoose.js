bfe2fb0e30cefc6197ee943aed2f4faf3aa10b30
Adam Hughes <adam.hughes@coda.global>
2017-12-14 15:21:46 -0600

    adjusting for loggin

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -7,4 +7,6 @@ mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 //mongodb://oryulin:qwer4567@ds044577.mlab.com:44577/todos
 
+console.log('Connecting to database:',process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp')
+
 module.exports = {mongoose};

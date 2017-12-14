4cce32613d3deb741d3685720b08c6c1a93bc43d
Adam Hughes <adam.hughes@coda.global>
2017-12-14 12:44:27 -0600

    Adds get path and test

--- server/server.js
+++ server/server.js
@@ -24,7 +24,11 @@ app.post('/todos', (req, res) => {
 });
 
 app.get('/todos', (req, res) => {
-
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (err) => {
+    res.status(400).send(err);
+  });
 });
 
 app.get('/todo', (req, res) => {
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,48 +5,78 @@ const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 const {User} = require('./../models/user');
 
+const todos = [{
+  text: "test1"
+  //first object
+}, {
+  text: "test2"
+  //second object
+}];
+
+// This deletes all items in the database before utilizing the test cases:
 beforeEach((done) => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
 });
 
 describe('POST /todos', () => {
-  it('should create a new todo', (done) => {
-    var text = 'placeholder';
+  describe('Posting valid data', () => {
+    it('should create a new todo', (done) => {
+      var text = 'placeholder';
 
-    request(app)
-      .post('/todos')
-      .send({text})
-      .expect(200)
-      .expect((res) => {
-        expect(res.body.text).toBe(text);
-      })
-      .end((err, res) => {
-        if(err) {
-          return done(err);
-        }
+      request(app)
+        .post('/todos')
+        .send({text})
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.text).toBe(text);
+        })
+        .end((err, res) => {
+          if(err) {
+            return done(err);
+          }
 
-        Todo.find().then((todos) => {
-          expect(todos.length).toBe(1);
-          expect(todos[0].text).toBe(text);
-          done();
-        }).catch((e) => done(e));
-      })
+          Todo.find({text}).then((todos) => {
+            expect(todos.length).toBe(1);
+            expect(todos[0].text).toBe(text);
+            done();
+          }).catch((e) => done(e));
+        })
+    });
   });
 
-  it('should not create todo with invalid body data', (done) => {
-    request(app)
-      .post('/todos')
-      .send({})
-      .expect(400)
-      .end((err, res) => {
-        if(err) {
-          return done(err);
-        }
+  describe('Posting invalid data', () => {
+    it('should not create document due to invalid body data', (done) => {
+      request(app)
+        .post('/todos')
+        .send({})
+        .expect(400)
+        .end((err, res) => {
+          if(err) {
+            return done(err);
+          }
 
-        Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
-          done();
-        }).catch((e) => done(e));
-      });
+          Todo.find().then((todos) => {
+            expect(todos.length).toBe(2);
+            done();
+          }).catch((e) => done(e));
+        });
+    });
+  });
+});
+
+describe('GET /todos', () => {
+  describe('Gathering all todos', () => {
+    it('should retrieve all todos from the database', (done) => {
+      request(app)
+        .get('/todos')
+        .send()
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.todos.length).toBe(2);
+        })
+        .end(done());
+    })
   });
 });

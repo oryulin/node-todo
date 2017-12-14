10b688f189e6d87524d21e27010fbe9b322afef2
Adam Hughes <adam.hughes@coda.global>
2017-12-14 15:09:37 -0600

    Setup database for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 // This will take a few milliseconds...be mindful.
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+// mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://oryulin:qwer4567@ds044577.mlab.com:44577/todos' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};

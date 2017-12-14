f0da2e2befd4afefe710d0a8397e3eea5ae73168
Adam Hughes <adam.hughes@coda.global>
2017-12-14 15:44:23 -0600

    adjust

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 // This will take a few milliseconds...be mindful.
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
-mongoose.connect('mongodb://oryulin:qwer4567@ds139446.mlab.com:39446/todoapp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+// mongoose.connect('mongodb://oryulin:qwer4567@ds139446.mlab.com:39446/todoapp');
 //mongodb://<dbuser>:<dbpassword>@ds139446.mlab.com:39446/todoapp
 
 // console.log('Connecting to database:',process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp')
